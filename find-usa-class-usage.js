756f25a2f54f49c57341f39fa3a50352e1a5add1
Atul Varma <varmaa@gmail.com>
2017-08-16 14:22:59 -0400

    Show percentage of USWDS sites whose front page uses class at least once.

--- find-usa-class-usage.js
+++ find-usa-class-usage.js
@@ -1,27 +1,39 @@
 const markdownTable = require('markdown-table');
-const sites = require('./sites');
-const classes = {};
+const cachedSites = require('./sites').getCachedSync();
+const siteClasses = new Map();
 
-sites.getCachedSync().forEach(site => {
+cachedSites.forEach(site => {
   const html = site.getCacheSync();
   const re = /usa-([A-Za-z0-9\-_]+)/g;
+  let classes = new Map();
   let match;
 
   while (match = re.exec(html)) {
     const className = `usa-${match[1]}`;
-    if (!(className in classes)) {
-      classes[className] = 0;
-    }
-    classes[className] += 1;
+    classes.set(className, (classes.get(className) || 0) + 1);
+  }
+
+  for (let className of classes.keys()) {
+    siteClasses.set(className, (siteClasses.get(className) || 0) + 1);
   }
 });
 
-const sorted = Object.keys(classes).sort((a, b) => {
-  return classes[b] - classes[a];
+const sorted = Array.from(siteClasses.keys()).sort((a, b) => {
+  return siteClasses.get(b) - siteClasses.get(a);
 });
 
+const rows = sorted.map(className => [
+  className,
+  Math.floor(siteClasses.get(className) / cachedSites.length * 100) + '%',
+]);
+
 const table = markdownTable([
-  ['Class', 'Count'],
-].concat(sorted.map(className => [className, classes[className]])));
+  ['Class', 'Usage'],
+].concat(rows));
 
 console.log(table);
+
+console.log(
+  "\nUsage represents the percentage of USWDS sites whose front page " +
+  "uses the class at least once."
+);
